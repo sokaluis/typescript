0d99a7d6bd63ab2bbcd41b523039e4581b533465
Luis Azocar <sokaluis@gmail.com>
2021-05-13 21:57:18 -0300

    number

---
+++ number.js
@@ -0,0 +1,11 @@
+var avengers = 5;
+var villanos;
+var otros = 2;
+if (avengers > villanos) {
+    console.log('Estamos salvados');
+}
+else {
+    console.log('Estamos muertos');
+}
+otros = 123;
+otros = 122.32;
---
+++ number.ts
@@ -0,0 +1,12 @@
+let avengers: number = 5;
+let villanos: number;
+let otros = 2;
+
+if(avengers > villanos) {
+    console.log('Estamos salvados')
+} else {
+    console.log('Estamos muertos')
+}
+
+otros = 123;
+otros = 122.32;
