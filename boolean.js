04af25f5ed807c62da43254d780548dc68635246
Luis Azocar <sokaluis@gmail.com>
2021-05-13 21:56:44 -0300

    boolean

---
+++ boolean.js
@@ -0,0 +1,13 @@
+var esSuperman = true;
+var esBatman;
+var esAquaman = false;
+if (esSuperman) {
+    console.log('Estamos salvados');
+}
+else {
+    console.log('Opps... Nop');
+}
+function convertirClark() {
+    return true;
+}
+esSuperman = convertirClark();
---
+++ boolean.ts
@@ -0,0 +1,15 @@
+let esSuperman: boolean = true;
+let esBatman: boolean;
+let esAquaman: boolean = false;
+
+if (esSuperman) {
+    console.log('Estamos salvados')
+} else {
+    console.log('Opps... Nop')
+}
+
+function convertirClark() {
+    return true;
+}
+
+esSuperman = convertirClark();
