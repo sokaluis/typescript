38f08f356540879ac2c72ba37ac8d04bde1f11f5
Luis Azocar <sokaluis@gmail.com>
2021-05-13 21:58:06 -0300

    Section 2: Strings

---
+++ string.js
@@ -0,0 +1,10 @@
+var batman = 'Batman';
+var linternaVerde = 'Linterna Verde';
+var volcanNegro = 'Volcán Negro';
+console.log(batman);
+console.log(linternaVerde);
+console.log(volcanNegro);
+var concatenar = "Los héroes son:" + batman + ', ' + linternaVerde + volcanNegro;
+var concat = "Los h\u00E9roes son: " + batman + ", " + linternaVerde + ", " + volcanNegro;
+console.log(concatenar);
+console.log(concat);
---
+++ string.ts
@@ -0,0 +1,13 @@
+let batman: string = 'Batman';
+let linternaVerde: string = 'Linterna Verde';
+let volcanNegro: string = 'Volcán Negro';
+
+console.log(batman);
+console.log(linternaVerde);
+console.log(volcanNegro);
+
+let concatenar: string = "Los héroes son:" + batman + ', ' + linternaVerde + volcanNegro;
+let concat: string = `Los héroes son: ${batman}, ${linternaVerde}, ${volcanNegro}`;
+
+console.log(concatenar);
+console.log(concat);
