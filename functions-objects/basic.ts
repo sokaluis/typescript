1956992b5073a7c12b482036a95724338c683ff5
Luis Azocar <sokaluis@gmail.com>
2021-05-16 19:43:45 -0300

    Functions - Basics

---
+++ functions-objects/basic.ts
@@ -0,0 +1,12 @@
+let heroe: string = "Flash";
+
+function hereo_name(): string {
+    return heroe;
+}
+
+let signal_batman = (): string => {
+    return "Batman's Signal actived,";
+}
+
+console.log( hereo_name() );
+console.log( signal_batman() );
---
+++ ts-built/functions-objects/basic.js
@@ -0,0 +1,10 @@
+"use strict";
+var heroe = "Flash";
+function hereo_name() {
+    return heroe;
+}
+var signal_batman = function () {
+    return "Batman's Signal actived,";
+};
+console.log(hereo_name());
+console.log(signal_batman());
