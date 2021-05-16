79470af602bbf7ae0e1ca83ac3202dd90fc8d3b4
Luis Azocar <sokaluis@gmail.com>
2021-05-16 20:17:35 -0300

    Functions - Parameters 3.0

--- functions-objects/parameters.ts
+++ functions-objects/parameters.ts
@@ -18,8 +18,13 @@ function toCapitalize(word: string): string {
     return `${word.charAt(0).toLocaleUpperCase()} ${word.substr(1).toLocaleLowerCase()}`;
 }
 
+function nombreRest(nombre: string, ...rest: string[]): string {
+    return `${nombre} ${rest.join(' ')}`;
+}
+
 let nombreCompleto: string = nombreObligatorio("Clark", "Kent", true);
 let nombre: string = nombreOpcional("Clark", "Kent");
+let apiNames: string = nombreRest("Clark", "Kent", "Otros", "Casas");
 
 console.log(nombreCompleto);
 console.log(nombre);
--- ts-built/functions-objects/parameters.js
+++ ts-built/functions-objects/parameters.js
@@ -19,7 +19,15 @@ function nombreOpcional(nombre, apellido) {
 function toCapitalize(word) {
     return word.charAt(0).toLocaleUpperCase() + " " + word.substr(1).toLocaleLowerCase();
 }
+function nombreRest(nombre) {
+    var rest = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        rest[_i - 1] = arguments[_i];
+    }
+    return nombre + " " + rest.join(' ');
+}
 var nombreCompleto = nombreObligatorio("Clark", "Kent", true);
 var nombre = nombreOpcional("Clark", "Kent");
+var apiNames = nombreRest("Clark", "Kent", "Otros", "Casas");
 console.log(nombreCompleto);
 console.log(nombre);
