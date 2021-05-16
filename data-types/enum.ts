51823080d0230a7f4fa1ee40ec7a3e42d8cd6491
Luis Azocar <sokaluis@gmail.com>
2021-05-16 15:03:20 -0300

    Data Type: enum

---
+++ data-types/enum.ts
@@ -0,0 +1,9 @@
+enum Volumen {
+    min = 1,
+    medium = 5,
+    max = 10,
+};
+
+let audio: number = Volumen.min;
+
+console.log( Audio );
---
+++ ts-built/data-types/enum.js
@@ -0,0 +1,10 @@
+"use strict";
+var Volumen;
+(function (Volumen) {
+    Volumen[Volumen["min"] = 1] = "min";
+    Volumen[Volumen["medium"] = 5] = "medium";
+    Volumen[Volumen["max"] = 10] = "max";
+})(Volumen || (Volumen = {}));
+;
+var audio = Volumen.min;
+console.log(Audio);
