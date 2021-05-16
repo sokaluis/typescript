e9f93bbb2e021cbcd56d9afe279e80cf9c0db53a
Luis Azocar <sokaluis@gmail.com>
2021-05-16 13:54:35 -0300

    solved typescript error

--- data-types/number.ts
+++ data-types/number.ts
@@ -2,11 +2,11 @@ let avengers: number = 5;
 let villanos: number;
 let otros = 2;
 
-if(avengers > villanos) {
-    console.log('Estamos salvados')
-} else {
-    console.log('Estamos muertos')
-}
+// if(avengers > villanos) {
+//     console.log('Estamos salvados')
+// } else {
+//     console.log('Estamos muertos')
+// }
 
 otros = 123;
 otros = 122.32;
