e48c103e5910f5ac3a8fa10bbb1a5c7ac5fdd124
Luis Azocar <sokaluis@gmail.com>
2021-05-16 15:50:01 -0300

    Data Type: Null / Undefined

---
+++ data-types/null-undefined.ts
@@ -0,0 +1,4 @@
+// if strictNullChecks is true you can't assing null / undefined
+
+// let ironman: null = undefined; // You can assing the type undefined or null to other types
+// ironman = null;
---
+++ ts-built/data-types/null-undefined.js
@@ -0,0 +1,4 @@
+"use strict";
+// if strictNullChecks is true you can't assing null / undefined
+// let ironman: null = undefined; // You can assing the type undefined or null to other types
+// ironman = null;
