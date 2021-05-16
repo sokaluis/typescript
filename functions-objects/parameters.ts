2f1aba955f3890fdf81ddbb66cfe6d85a9159859
Luis Azocar <sokaluis@gmail.com>
2021-05-16 20:08:20 -0300

    Functions - Parameters 2.0

--- functions-objects/parameters.ts
+++ functions-objects/parameters.ts
@@ -1,7 +1,25 @@
-function nombreCompleto ( nombre: string, apellido: string ): string {
-    return `${nombre} ${apellido}`;
+function nombreObligatorio(nombre: string, apellido: string, capitalize: boolean = false): string {
+    if (capitalize) {
+        return `${toCapitalize(nombre)} ${toCapitalize(apellido)}`
+    } else {
+        return `${nombre} ${apellido}`;
+    }
 }
 
-let nombre: string = nombreCompleto("Clark", "Kent");
+function nombreOpcional(nombre: string, apellido?: string): string {
+    if (apellido) {
+        return ` Hola ${nombre} ${apellido}`;
+    } else {
+        return ` Hola ${nombre}`;
+    }
+}
 
+function toCapitalize(word: string): string {
+    return `${word.charAt(0).toLocaleUpperCase()} ${word.substr(1).toLocaleLowerCase()}`;
+}
+
+let nombreCompleto: string = nombreObligatorio("Clark", "Kent", true);
+let nombre: string = nombreOpcional("Clark", "Kent");
+
+console.log(nombreCompleto);
 console.log(nombre);
--- ts-built/functions-objects/parameters.js
+++ ts-built/functions-objects/parameters.js
@@ -1,6 +1,25 @@
 "use strict";
-function nombreCompleto(nombre, apellido) {
-    return nombre + " " + apellido;
+function nombreObligatorio(nombre, apellido, capitalize) {
+    if (capitalize === void 0) { capitalize = false; }
+    if (capitalize) {
+        return toCapitalize(nombre) + " " + toCapitalize(apellido);
+    }
+    else {
+        return nombre + " " + apellido;
+    }
 }
-var nombre = nombreCompleto("Clark", "Kent");
+function nombreOpcional(nombre, apellido) {
+    if (apellido) {
+        return " Hola " + nombre + " " + apellido;
+    }
+    else {
+        return " Hola " + nombre;
+    }
+}
+function toCapitalize(word) {
+    return word.charAt(0).toLocaleUpperCase() + " " + word.substr(1).toLocaleLowerCase();
+}
+var nombreCompleto = nombreObligatorio("Clark", "Kent", true);
+var nombre = nombreOpcional("Clark", "Kent");
+console.log(nombreCompleto);
 console.log(nombre);
