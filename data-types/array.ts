18b28c49917a45a3691a5c6d7e1fa8175cdc99ac
Luis Azocar <sokaluis@gmail.com>
2021-05-16 14:55:12 -0300

    Data Type: array

---
+++ data-types/array.ts
@@ -0,0 +1,6 @@
+let arreglo: number[] = [1,2,3,4,5,6,7,8];
+let vil: string[] = ["Omega Rojo", "Dormamu", "Duende Verde"];
+
+console.log( arreglo[0].toFixed(2) );
+
+console.log( vil[0].charAt(0) );
