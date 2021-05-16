5ed56eb947818b8b18d998fc04c5c19fe839af38
Luis Azocar <sokaluis@gmail.com>
2021-05-16 14:55:42 -0300

    Data Type: tuple

---
+++ data-types/tuples.ts
@@ -0,0 +1,5 @@
+let heroes: [string, number, boolean] = ["Dr Strange", 100, true];
+
+heroes.push(true);
+
+console.log(heroes);
---
+++ ts-built/data-types/tuples.js
@@ -0,0 +1,4 @@
+"use strict";
+var heroes = ["Dr Strange", 100, true];
+heroes.push(true);
+console.log(heroes);
