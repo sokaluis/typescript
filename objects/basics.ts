dd91c373301491a9500ccc6bc5a83489b444749f
Luis Azocar <sokaluis@gmail.com>
2021-05-16 22:15:28 -0300

    Objects - types 1.0

--- objects/basics.ts
+++ objects/basics.ts
@@ -1,12 +1,23 @@
 
-let flash = {
+let flash: {
+	name: string,
+	age: number,
+	powers: string[],
+	getName: () => string,
+} = {
 	name: "Barry Allen",
 	age: 25,
 	powers: ["Run", "Time travel"],
+	getName() {
+		return this.name;
+	},
 };
 
 flash = {
 	name: "Superman",
 	age: 500,
 	powers: ["Fly"],
+	getName() {
+		return this.name;
+	},
 }
--- ts-built/objects/basics.js
+++ ts-built/objects/basics.js
@@ -3,9 +3,15 @@ var flash = {
     name: "Barry Allen",
     age: 25,
     powers: ["Run", "Time travel"],
+    getName: function () {
+        return this.name;
+    },
 };
 flash = {
     name: "Superman",
     age: 500,
     powers: ["Fly"],
+    getName: function () {
+        return this.name;
+    },
 };
